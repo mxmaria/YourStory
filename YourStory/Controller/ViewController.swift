dde053be59b99e110bb964b0adc57975c2c34334
mxmaria <mapecheritsyna@gmail.com>
2021-03-08 23:47:51 +0300

    Added YourStory proj

---
+++ YourStory/Controller/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  YourStory
+//
+//  Created by Maria Pecheritsyna on 05.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+                updateUI()
+    }
+    
+    func updateUI() {
+            storyLabel.text = storyBrain.getStoryTitle()
+            choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+            choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+        }
+    
+}
+
---
+++ YourStory/Model/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  YourStory
+//
+//  Created by Maria Pecheritsyna on 05.03.2021.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
